e652bfa1c35de1f520a1a8bb7393462631393f7c
Nick <buckley183@gmail.com>
2018-06-03 21:58:28 -0400

    Initial commit. Base version of pong made.

---
+++ Crafty/Game1.cs
@@ -0,0 +1,354 @@
+﻿//Comment out for prod
+//#define DEBUGINFO
+
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Crafty
+{
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        Rectangle player1;
+        Color[] player1Color;
+        Texture2D player1Tex;
+        int player1Score = 0;
+
+        Rectangle player2;
+        Color[] player2Color;
+        Texture2D player2Tex;
+        int player2Score = 0;
+
+        Rectangle ball;
+        Color[] ballColor;
+        Texture2D ballTex;
+        int ballVX;
+        int ballVY;
+
+        SpriteFont font;
+
+        // At the top of your class:
+        Texture2D pixel;
+
+        public Game1()
+        {
+            //Should move this to a settings system
+            graphics = new GraphicsDeviceManager(this);
+            graphics.PreferredBackBufferHeight = 800;
+            graphics.PreferredBackBufferWidth = 1200;
+            Content.RootDirectory = "Content";
+            this.IsMouseVisible = true;
+
+            player1 = new Rectangle
+            {
+                X = 100,
+                Y = 400,
+                Width = 50,
+                Height = 100
+            };
+            player2 = new Rectangle
+            {
+                X = 1100,
+                Y = 400,
+                Width = 50,
+                Height = 100
+            };
+            ball = new Rectangle
+            {
+                X = graphics.PreferredBackBufferWidth / 2 - 30,
+                Y = graphics.PreferredBackBufferHeight / 2 - 30,
+                Width = 30,
+                Height = 30
+            };
+        }
+
+        protected override void Initialize()
+        {
+            //Init player settings. Should be in an entity with an input system attached
+            player1Color = new Color[player1.Width * player1.Height];
+            player1Tex = new Texture2D(GraphicsDevice, player1.Width, player1.Height);
+
+            player2Color = new Color[player2.Width * player2.Height];
+            player2Tex = new Texture2D(GraphicsDevice, player2.Width, player2.Height);
+
+            for (var i = 0; i < player1Color.Length; i++)
+            {
+                player1Color[i] = Color.White;
+                player2Color[i] = Color.White;
+            }
+
+            player1Tex.SetData(player1Color);
+            player2Tex.SetData(player2Color);
+
+            ballColor = new Color[ball.Width * ball.Height];
+            ballTex = new Texture2D(GraphicsDevice, ball.Width, ball.Height);
+
+            //Creating the ball
+            var radius = 30;
+            var diam = radius / 2f;
+            var diamsq = diam * diam;
+
+            for (int x = 0; x < radius; x++)
+            {
+                for (int y = 0; y < radius; y++)
+                {
+                    int index = x * radius + y;
+                    Vector2 pos = new Vector2(x - diam, y - diam);
+                    if (pos.LengthSquared() <= diamsq)
+                        ballColor[index] = Color.White;
+                    else
+                        ballColor[index] = Color.Transparent;
+                }
+            }
+
+            ballTex.SetData(ballColor);
+
+            var rand = new Random();
+            var num = rand.Next(0, 50);
+
+            ballVX = num > 25 ? rand.Next(5, 15) : rand.Next(-15, -5);
+            ballVY = num > 25 ? rand.Next(5, 10) : rand.Next(-10, -5);
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            font = Content.Load<SpriteFont>("Font/Debug");
+
+            // Somewhere in your LoadContent() method:
+            pixel = new Texture2D(GraphicsDevice, 1, 1, false, SurfaceFormat.Color);
+            pixel.SetData(new[] { Color.White }); // so that we can draw whatever color we want on top of it 
+        }
+
+        protected override void UnloadContent()
+        {
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            //Player 1 Up. Should refactor into Input system on each player controller
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
+            {
+                if (player1.Y - 10 <= 0)
+                    player1.Y = 0;
+                else
+                    player1.Y -= 10;
+            }
+            //Player 1 Down
+            if (Keyboard.GetState().IsKeyDown(Keys.S))
+            {
+                if (player1.Y + player1.Height + 10 >= graphics.PreferredBackBufferHeight)
+                    player1.Y = graphics.PreferredBackBufferHeight - player1.Height;
+                else
+                    player1.Y += 10;
+            }
+            //Player 2 Up
+            if (Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+                if (player2.Y - 10 <= 0)
+                    player2.Y = 0;
+                else
+                    player2.Y -= 10;
+            }
+            //Player 2 Down
+            if (Keyboard.GetState().IsKeyDown(Keys.Down))
+            {
+                if (player2.Y + player2.Height + 10 >= graphics.PreferredBackBufferHeight)
+                    player2.Y = graphics.PreferredBackBufferHeight - player2.Height;
+                else
+                    player2.Y += 10;
+            }
+
+            if (!ball.Intersects(player1))
+            {
+                if (!ball.Intersects(player2))
+                {
+                    if (ball.Right >= graphics.PreferredBackBufferWidth)
+                    {
+                        ball.X = graphics.PreferredBackBufferWidth - ball.Width;
+                        ballVX = -ballVX;
+                        ball.X += ballVX;
+                        player1Score++;
+                    }
+                    else if (ball.Right <= 0)
+                    {
+                        ball.X = 0 + ball.Width;
+                        ballVX = -ballVX;
+                        ball.X += ballVX;
+                        player2Score++;
+                    }
+                    else if (ball.Bottom <= 0)
+                    {
+                        ball.Y = 0 + ball.Height;
+                        ballVY = -ballVY;
+                        ball.Y += ballVY;
+                    }
+                    else if (ball.Top >= graphics.PreferredBackBufferHeight)
+                    {
+                        ball.Y = graphics.PreferredBackBufferHeight - ball.Height;
+                        ballVY = -ballVY;
+                        ball.Y += ballVY;
+                    }
+                    else
+                    {
+                        ball.X += ballVX;
+                        ball.Y += ballVY;
+                    }
+                }
+                else
+                {
+                    //Checking for top hit
+                    if (ball.Top == player2.Top)
+                    {
+                        //if true it means its a top left corner hit
+                        if (ball.Right >= player2.Left || ball.Left <= player2.Right)
+                        {
+                            ball.X = ball.Right >= player2.Left ? player2.Left - 5 : player2.Right + 5;
+                            ballVX = -ballVX;
+                            ball.X += ballVX;
+                        }
+                        else
+                        {
+                            //top hit
+                            ballVY = -ballVY;
+                            ball.Y += ballVY;
+                        }
+                    }
+                    //Checking for bottom hit
+                    else if (ball.Bottom == player2.Bottom)
+                    {
+                        //if true it means its a bottom corner hit
+                        if (ball.Right >= player2.Left || ball.Left <= player2.Right)
+                        {
+                            ball.X = ball.Right >= player2.Left ? player2.Left - 5 : player2.Right + 5;
+                            ballVX = -ballVX;
+                            ball.X += ballVX;
+                        }
+                        else
+                        {
+                            //bottom hit
+                            ballVY = -ballVY;
+                            ball.Y += ballVY;
+                        }
+                    }
+                    else
+                    {
+                        ballVX = -ballVX;
+                        ball.X += ballVX;
+                        ballVY = -ballVY;
+                        ball.Y += ballVY;
+                    }
+                }
+            }
+            else
+            {
+                //Checking for top hit
+                if (ball.Top == player1.Top)
+                {
+                    //if true it means its a top left corner hit
+                    if (ball.Right >= player1.Left || ball.Left <= player1.Right)
+                    {
+                        ball.X = ball.Right >= player1.Left ? player1.Left - 5 : player1.Right + 5;
+                        ballVX = -ballVX;
+                        ball.X += ballVX;
+                    }
+                    else
+                    {
+                        //top hit
+                        ballVY = -ballVY;
+                        ball.Y += ballVY;
+                    }
+                }
+                //Checking for bottom hit
+                else if (ball.Bottom == player1.Bottom)
+                {
+                    //if true it means its a bottom corner hit
+                    if (ball.Right >= player1.Left || ball.Left <= player1.Right)
+                    {
+                        ball.X = ball.Right >= player1.Left ? player1.Left - 5 : player1.Right + 5;
+                        ballVX = -ballVX;
+                        ball.X += ballVX;
+                    }
+                    else
+                    {
+                        //bottom hit
+                        ballVY = -ballVY;
+                        ball.Y += ballVY;
+                    }
+                }
+                else
+                {
+                    ballVX = -ballVX;
+                    ball.X += ballVX;
+                    ballVY = -ballVY;
+                    ball.Y += ballVY;
+                }
+            }
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            spriteBatch.Begin();
+
+            spriteBatch.Draw(player1Tex, player1, Color.White);
+            spriteBatch.Draw(player2Tex, player2, Color.White);
+            spriteBatch.Draw(ballTex, ball, Color.White);
+
+
+#if DEBUGINFO
+            spriteBatch.DrawString(font, $"Player1 X: {player1.X}", new Vector2(10, 10), Color.Green);
+            spriteBatch.DrawString(font, $"Player1 Y: {player1.Y}", new Vector2(10, 30), Color.Green);
+            spriteBatch.DrawString(font, $"Player2 X: {player2.X}", new Vector2(10, 60), Color.Green);
+            spriteBatch.DrawString(font, $"Player2 Y: {player2.Y}", new Vector2(10, 80), Color.Green);
+            spriteBatch.DrawString(font, $"Ball X: {ball.X}", new Vector2(10, 100), Color.Green);
+            spriteBatch.DrawString(font, $"Ball Y: {ball.Y}", new Vector2(10, 120), Color.Green);
+            spriteBatch.DrawString(font, $"Ball VX: {ballVX}", new Vector2(10, 140), Color.Green);
+            spriteBatch.DrawString(font, $"Ball VY: {ballVY}", new Vector2(10, 160), Color.Green);
+            DrawBorder(ball, 1, Color.Green);
+            spriteBatch.DrawString(font, $"Mouse X: {Mouse.GetState().X}", new Vector2(10, 200), Color.Green);
+            spriteBatch.DrawString(font, $"Mouse Y: {Mouse.GetState().Y}", new Vector2(10, 220), Color.Green);
+#endif
+
+            spriteBatch.DrawString(font, $"Player 1 Score: {player1Score}", new Vector2(40, 20), Color.Green);
+            spriteBatch.DrawString(font, $"Player 2 Score: {player2Score}", new Vector2(1040, 20), Color.Green);
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+        private void DrawBorder(Rectangle rectangleToDraw, int thicknessOfBorder, Color borderColor)
+        {
+            // Draw top line
+            spriteBatch.Draw(pixel, new Rectangle(rectangleToDraw.X, rectangleToDraw.Y, rectangleToDraw.Width, thicknessOfBorder), borderColor);
+
+            // Draw left line
+            spriteBatch.Draw(pixel, new Rectangle(rectangleToDraw.X, rectangleToDraw.Y, thicknessOfBorder, rectangleToDraw.Height), borderColor);
+
+            // Draw right line
+            spriteBatch.Draw(pixel, new Rectangle((rectangleToDraw.X + rectangleToDraw.Width - thicknessOfBorder),
+                                            rectangleToDraw.Y,
+                                            thicknessOfBorder,
+                                            rectangleToDraw.Height), borderColor);
+            // Draw bottom line
+            spriteBatch.Draw(pixel, new Rectangle(rectangleToDraw.X,
+                                            rectangleToDraw.Y + rectangleToDraw.Height - thicknessOfBorder,
+                                            rectangleToDraw.Width,
+                                            thicknessOfBorder), borderColor);
+        }
+    }
+}
